5c9a7feb0d0ef0c555cf29e80cd1c848b221e3cc
Andrew Marconi <andrew@marconimedia.com>
2014-12-25 17:28:46 -0500

    Added publishing script and test ttv plugin

---
+++ tools/publish-repo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SRC=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+SRC=$(dirname "${SRC}")"/repo/"
+DST="andrew@s17985321.onlinehome-server.com:/var/www/sites/com_marconimedia_kodi"
+echo "Synchronizing:"
+echo "--> SRC: ${SRC}"
+echo "--> DST: ${DST}"
+rsync -avzhe ssh --progress "${SRC}" "${DST}"
