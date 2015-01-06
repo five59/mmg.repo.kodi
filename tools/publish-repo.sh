188495ec9495b96928397457dcb3bfe993857a57
Andrew Marconi <andrew@marconimedia.com>
2015-01-05 19:09:53 -0500

    Delete publish-repo.sh

--- tools/publish-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-SRC=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-SRC=$(dirname "${SRC}")"/web/"
-DST="andrew@s17985321.onlinehome-server.com:/var/www/sites/com_marconimedia_kodi"
-echo "Synchronizing:"
-echo "--> SRC: ${SRC}"
-echo "--> DST: ${DST}"
-rsync -avzhe ssh --progress --delete "${SRC}" "${DST}"
